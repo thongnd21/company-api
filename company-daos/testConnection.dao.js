19cd40aa64c14c71a4cabd7de10009bd5f9052f6
Thanh <phanvanthanhbtx@gmail.com>
2020-07-23 16:58:53 +0700

    update

--- company-daos/testConnection.dao.js
+++ company-daos/testConnection.dao.js
@@ -1,16 +1,26 @@
 const db = require("../config/company-connection");
 const { check } = require("express-validator");
+const department = require("../companyModels/Department");
 module.exports = {
     checkConnection: async (dbInfo) => {
         try {
             var checkConnect = false;
-            return await db.dbConnection(dbInfo).authenticate()
+            var dep = await department.DepModel(db.dbConnection(dbInfo));
+            await db.dbConnection(dbInfo).authenticate()
             .then(() =>
                 {
                     checkConnect = true;
-                    return checkConnect;
+                    // return checkConnect;
                 } 
             )
+            return await dep.findAll({
+                where: { status_id: 1 }
+                
+            }).then(async res => {
+                console.log(res[0]);
+                return true;
+            })
+            
         } catch (err) {
             console.log(err);
         }
