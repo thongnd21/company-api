c714e23d7ad97f83c24874e8085320aa121ee378
thanh phan <thanhpvse62274@fpt.edu.vn>
2020-07-24 19:55:44 +0700

    edit

--- company-daos/employees.dao.js
+++ company-daos/employees.dao.js
@@ -5,14 +5,14 @@ const db = require("../config/company-connection");
 const Team = require('../companyModels/Team');
 
 module.exports = {
-    getAllEmployee : async (dbInfo) => {
+    getAllEmployee: async (dbInfo) => {
         try {
             var dep = Department.DepModel(db.dbConnection(dbInfo));
             var emp = Employee.EmpModel(db.dbConnection(dbInfo));
             db.dbConnection(dbInfo).authenticate()
-            .then(() => 
-            console.log("db connected")
-            )
+                .then(() =>
+                    console.log("db connected")
+                )
             return await emp.findAll(
                 {
                     include: [{
@@ -22,6 +22,38 @@ module.exports = {
                     where: { status_id: contants.EMPLOYEE_STATUS_ACTIVE }
                 }
             )
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getAllEmployeeToCheck: async (dbInfo) => {
+        try {
+            var dep = Department.DepModel(db.dbConnection(dbInfo));
+            var emp = Employee.EmpModel(db.dbConnection(dbInfo));
+            db.dbConnection(dbInfo).authenticate()
+                .then(() =>
+                    console.log("db connected")
+                )
+            return await emp.findAll({
+                attributes: ['id', 'primary_email', 'personal_email',
+                    'first_name', 'last_name', 'modified_date', 'address',
+                    'department_id', 'phone', 'status_id'],
+                include: [
+                    {
+                        model: dep,
+                        attributes: ['id', 'name'],
+                        where: {
+                            status_id: 1
+                        }
+                    }
+                ],
+                order: [
+                    ['primary_email', 'ASC']
+                ]
+            }).then(async res => {
+                console.log(res)
+                return res
+            });
         } catch (err) {
             console.log(err);
         }
