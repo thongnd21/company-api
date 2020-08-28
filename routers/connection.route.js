b88ac3fa09602d6a126190adbccced6c946e2f49
Thanh <phanvanthanhbtx@gmail.com>
2020-08-29 02:48:15 +0700

    update check connection

--- routers/connection.route.js
+++ routers/connection.route.js
@@ -16,42 +16,87 @@ router.post("/", async (req, res) => {
     try {
 
         var structure = {
-            employees: [],
-            teams: [],
-            departments: [],
-            positions: [],
+            employees: {},
+            teams: {},
+            departments: {},
+            positions: {},
+            team_employee: {},
             checkConnection: {
-                status : '',
-                message : ''
+                status: '',
+                message: ''
             }
         }
+        console.log(dbInfo);
         var check = await testConnectionDao.checkConnection(dbInfo);
         if (check === true) {
 
             //get employees
             structure.checkConnection.status = "success";
-            var employeeResponse = await employee_dao.getAllEmployeeToCheck(dbInfo);
-            console.log("----Get all employee from HRMS---");
-
-            structure.employees = [...structure.employees, ...employeeResponse];
-
-            var teamResponse = await team_dao.findAllTeamToCheck(dbInfo);
-
-            structure.teams = [...structure.teams, ...teamResponse];
-
-            // get derpartments
-            var departmentResponse = await department_dao.findAllDepartmentToCheck(dbInfo);
-            await departmentResponse.map(item => {
-                structure.departments.push(item);
-            });
-            //get positon
-            var positionResponse = await position.findAllPositionToCheck(dbInfo);
-            await positionResponse.map(item => {
-                structure.positions.push(item);
+            const connectionString = sql_connection.createConnection({
+                host: dbInfo.host,
+                user: dbInfo.username,
+                password: dbInfo.password,
+                database: dbInfo.dbName,
+                port: dbInfo.port,
+                timestamps: false,
+                pool: {
+                    max: 5,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                }
             })
+            // connect db
+            await connectionString.connect(function (err) {
+                if (err) throw err;
+                console.log('error when connecting to db:', err);
+                // setTimeout(checkConnection(dbInfo), 2000);
+            });
+            // on connect if db has been losed, we will reconnect
+            // connectionString.on('error', function (err) {
+            //     console.log('db error', err);
+            //     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            //         console.log(dbInfo);
+            //         checkConnection(newConnection);
+            //     } else {
+            //         throw err;
+            //     }
+            // });
+            // after connect, query get all information of tabel in db
+           
+            await connectionString.query('select * from information_schema.columns where table_schema = ' + '"' + dbInfo.dbName + '"' + ' order by table_name,ordinal_position', (err, result, fields) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(result[1]);
+                for (var i = 0; i < result.length; i++) {
+                    if (result[i].TABLE_NAME == "employee") {
+                        structure.employees[result[i].COLUMN_NAME] = (result[i].COLUMN_NAME)
+                    }
+                    if (result[i].TABLE_NAME == "department") {
+                        structure.departments[result[i].COLUMN_NAME] = (result[i].COLUMN_NAME)
+                    }
+                    if (result[i].TABLE_NAME == "team") {
+                        structure.teams[result[i].COLUMN_NAME] = (result[i].COLUMN_NAME)
+                    }
+                    if (result[i].TABLE_NAME == "team_employee") {
+                        structure.team_employee[result[i].COLUMN_NAME] = (result[i].COLUMN_NAME)
+                    }
+                    if (result[i].TABLE_NAME == "position") {
+                        structure.positions[result[i].COLUMN_NAME] = (result[i].COLUMN_NAME)
+                    }
+                }
 
 
-            res.json(structure);
+                connectionString.destroy(function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+                console.log(structure);
+                res.json(structure);
+                return;
+            })
 
 
         } else {
@@ -59,16 +104,16 @@ router.post("/", async (req, res) => {
             if (check.original.code == "ER_DBACCESS_DENIED_ERROR") {
                 structure.checkConnection.status = "fail";
                 structure.checkConnection.message = "Wrong connection name, please input again!";
-            }else if(check.original.code ==  "ENOTFOUND"){
+            } else if (check.original.code == "ENOTFOUND") {
                 structure.checkConnection.status = "fail";
                 structure.checkConnection.message = "Wrong host, please input again!";
-            }else if(check.original.code ==  "ETIMEDOUT"){
+            } else if (check.original.code == "ETIMEDOUT") {
                 structure.checkConnection.status = "fail";
                 structure.checkConnection.message = "Wrong port, please input again!";
-            }else if(check.original.code ==  "ER_ACCESS_DENIED_ERROR"){
+            } else if (check.original.code == "ER_ACCESS_DENIED_ERROR") {
                 structure.checkConnection.status = "fail";
                 structure.checkConnection.message = "Wrong user name or password, please input again!";
-            }else{
+            } else {
                 structure.checkConnection.status = "fail";
                 structure.checkConnection.message = "Can not connect, please input again!";
             }
